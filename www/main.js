13acb16c608db4717067d57e51640277151eae8c
Praveen Kumar Pendyala <praveendath92@gmail.com>
2017-01-03 22:38:42 +0100

    a simple angularjs to show a graph

---
+++ www/main.js
@@ -0,0 +1,35 @@
+angular.module('newYearGoalsApp', ['chart.js']);
+
+angular.module('newYearGoalsApp').controller('MainController', function($scope, $http) {
+	// Get data
+    var req = {
+        method: 'GET',
+        url: 'distance.json'
+    };
+    $http(req)
+        .then(
+        function (response) { // Success callback
+            raw_data = response.data;
+
+            // Let's process now!
+            label_days = []
+            target_dists = []
+            actual_dists = []
+            for (var i = 0; i < raw_data.length; i++) {
+            	label_days.push("Day " + i);
+            	target_dists.push( ((i+1)*1000/365).toFixed(1) );
+            	actual_dists.push( (raw_data[i]).toFixed(1) );
+            }
+
+            // show data to scope
+            $scope.labels = label_days;
+            $scope.series = ['Distance Ran', 'Target Distance'];
+            $scope.data = [actual_dists,target_dists];
+        },
+        function (response) { //Error callback
+            console.log(response.toString());
+        }
+    );
+
+
+});
