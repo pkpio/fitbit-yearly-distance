edab90f0f77d36eec08b1e01cf5231e82dfc866e
Praveen Kumar Pendyala <praveendath92@gmail.com>
2017-01-13 19:33:27 +0100

    Migrated to n3-charts for charting

--- www/main.js
+++ www/main.js
@@ -1,6 +1,61 @@
-angular.module('newYearGoalsApp', ['chart.js']);
+angular.module('newYearGoalsApp', ['n3-line-chart']);
 
 angular.module('newYearGoalsApp').controller('MainController', function($scope, $http) {
+
+    // Options for plot
+    $scope.options = {
+            margin: {
+            top: 5
+        },
+        series: [
+            {
+              axis: "y",
+              dataset: "distance_data",
+              key: "actual",
+              label: "Distance ran",
+              color: "rgb(70, 191, 189)",
+              type: ["dot","line","area"],
+              id: "mySeries0"
+            },
+            {
+              axis: "y",
+              dataset: "distance_data",
+              key: "target",
+              label: "Expected distance",
+              color: "rgb(247, 70, 74)",
+              type: ["dot","line","area"],
+              id: "mySeries1"
+            }
+        ],
+        axes: {
+            x: {
+              key: "day"
+            }
+        }
+    };
+
+    // Init with some dummy data
+    $scope.data = {
+        distance_data:[
+            {
+                day: 0,
+                actual: 2,
+                target: 2.7 
+            },
+            {
+                day: 1,
+                actual: 5,
+                target: 5.4 
+            },
+            {
+                day: 3,
+                actual: 9,
+                target: 8.1 
+            }
+        ]
+    };
+
+
 	// Get data
     var req = {
         method: 'GET',
@@ -12,19 +67,18 @@ angular.module('newYearGoalsApp').controller('MainController', function($scope, 
             raw_data = response.data;
 
             // Let's process now!
-            label_days = []
-            target_dists = []
-            actual_dists = []
+            distances = [];
             for (var i = 0; i < raw_data.length; i++) {
-            	label_days.push("Day " + (i+1));
-            	target_dists.push( ((i+1)*1000/365).toFixed(1) );
-            	actual_dists.push( (raw_data[i]).toFixed(1) );
+                point = {
+                    day: i,
+                    target: ((i+1)*1000/365).toFixed(1),
+                    actual: (raw_data[i]).toFixed(1)
+                };
+                distances.push(point);
             }
 
             // show data to scope
-            $scope.labels = label_days;
-            $scope.series = ['Distance Ran', 'Target Distance'];
-            $scope.data = [actual_dists,target_dists];
+            $scope.data = {distance_data:distances};
         },
         function (response) { //Error callback
             console.log(response.toString());
