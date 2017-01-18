023a487554bbccb215ff28f4cef6e12fd2807631
Praveen Kumar Pendyala <praveendath92@gmail.com>
2017-01-18 16:32:55 +0100

    Added another for tracking cummulative differences

--- www/main.js
+++ www/main.js
@@ -30,16 +30,25 @@ angular.module('newYearGoalsApp').controller('MainController', function($scope, 
             label_days = []
             target_dists = []
             actual_dists = []
+            diff_dists = []
             for (var i = 0; i < raw_data.length; i++) {
+                target = (i+1)*1000/365;
+                actual = raw_data[i];
+
                 label_days.push("Day " + (i+1));
-                target_dists.push( ((i+1)*1000/365).toFixed(1) );
-                actual_dists.push( (raw_data[i]).toFixed(1) );
+                target_dists.push(target.toFixed(1));
+                actual_dists.push(actual.toFixed(1));
+                diff_dists.push( (actual-target).toFixed(1) ) ;
             }
 
             // show data to scope
             $scope.labels = label_days;
             $scope.series = ['Distance Ran', 'Target Distance'];
             $scope.data = [actual_dists,target_dists];
+
+            $scope.diffLabels = label_days;
+            $scope.diffSeries = ['Difference'];
+            $scope.diffData = [diff_dists];
         },
         function (response) { //Error callback
             console.log(response.toString());
