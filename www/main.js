9b18e380a4abba8d8079ac7c282ca2b240cc4d20
Praveen Kumar Pendyala <praveendath92@gmail.com>
2017-01-18 16:43:13 +0100

    point hit radius set to 25

--- www/main.js
+++ www/main.js
@@ -13,7 +13,7 @@ angular.module('newYearGoalsApp').controller('MainController', function($scope, 
 
     $scope.datasetOverride = [{
         lineTension: 0,
-        pointHitRadius: 50
+        pointHitRadius: 25
     }]
 
 	// Get data
